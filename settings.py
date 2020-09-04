302c9ccab7f98f54bd112b728996a2fa12928b6e
TDK's Y7000p <793065367@qq.com>
2020-09-04 10:42:57 +0800

    add more func

--- settings.py
+++ settings.py
@@ -7,3 +7,11 @@ class Settings():
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        # 飞船设置
+        self.ship_speed_factor = 1
+
+        # 子弹设置
+        self.bullet_speed_factor = 0.8
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
